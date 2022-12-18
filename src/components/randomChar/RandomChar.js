f9246c2e390319d45a2d391e0113b65c10064bbf
Alexander Vasilevsky <vasilevskiysasha1@gmail.com>
2022-12-18 04:04:56 +0300

    Added img cover/contain

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -82,10 +82,18 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  const noPhotochar =
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg";
 
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img
+        src={thumbnail}
+        alt="Random character"
+        className={`randomchar__img  ${
+          thumbnail === noPhotochar ? null : "randomchar__noImg"
+        }`}
+      />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
