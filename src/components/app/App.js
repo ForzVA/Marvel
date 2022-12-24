7adbf539f53e3fbd2160ed25b8e63476c58387a1
Alexander Vasilevsky <vasilevskiysasha1@gmail.com>
2022-12-24 20:46:34 +0300

    React 17 => 18+(new feature -batching)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -19,7 +19,7 @@ const App = () => {
     <div className="app">
       <AppHeader />
       <main>
-        {/* <ErrorBoundary>
+        <ErrorBoundary>
           <RandomChar />
         </ErrorBoundary>
         <div className="char__content">
@@ -30,8 +30,8 @@ const App = () => {
             <CharInfo charId={selectedChar} />
           </ErrorBoundary>
         </div>
-        <img className="bg-decoration" src={decoration} alt="vision" /> */}
-        <ComicsList />
+        <img className="bg-decoration" src={decoration} alt="vision" />
+        {/* <ComicsList /> */}
       </main>
     </div>
   );
